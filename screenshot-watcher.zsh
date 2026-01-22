24f23c3184bc851077caf9ddf830e352355fd93c
Charles Mesa Cayobit <charlesmesacayobit@gmail.com>
2026-01-22 10:31:13 -0800

    Massive update

--- screenshot-watcher.zsh
+++ screenshot-watcher.zsh
@@ -12,17 +12,21 @@ zmodload zsh/files
 
 readonly SCRIPT_NAME=${0:t:r}
 
-readonly MOUNT_POINT="/Volumes/${SCRIPT_NAME}"
-integer -r DISK_SIZE=65536
-
-readonly LOCK_PATH="${MOUNT_POINT}/${SCRIPT_NAME}.lock"
-
-readonly HOMEBREW_PREFIX=/opt/homebrew
-
 readonly EXECUTABLE_DIR=${HOME}/.local/bin/screenshot-tagger
 readonly ARG_FILES_DIR=${HOME}/.local/share/exiftool
 
-float -r EXECUTION_DELAY=0.5
+if [[ -f ${EXECUTABLE_DIR}/config.zsh ]]; then
+    source "${EXECUTABLE_DIR}/config.zsh"
+else
+    print -u 2 -- 'Environment file not found; exiting...'
+    exit $EX_NOINPUT
+fi
+
+readonly LOCK_PATH="${TMPDIR}${SCRIPT_NAME}.lock"
+
+float -r EXECUTION_DELAY=1
+
+readonly HOMEBREW_PREFIX=/opt/homebrew
 
 export -Ua path
 path=(
@@ -33,11 +37,6 @@ path=(
 
 ################################################################################
 
-if [[ ! -d $MOUNT_POINT ]]; then
-    diskutil apfs create $(hdiutil attach -nomount ram://$DISK_SIZE) "${MOUNT_POINT:t}"
-    print -- "Created RAM disk: '${MOUNT_POINT}'"
-fi
-
 # Taking multiple screenshots in succession causes `launchd` to trigger the same
 # amount of times. Checking for this lock ensures that only the first instance
 # of the script executes the rest of the script body.
@@ -46,22 +45,15 @@ if mkdir -m 200 "$LOCK_PATH" 2>/dev/null; then
     print -- "Created lock in '${LOCK_PATH:h}/'"
 else
     print -u 2 -- "Lock exists in '${LOCK_PATH:h}/'; exiting..."
-    exit 1
+    exit $EX_TEMPFAIL
 fi
 
 sleep $EXECUTION_DELAY # Give time for all screenshots to be written to disk
 
-if [[ -f ${EXECUTABLE_DIR}/config.zsh ]]; then
-    source "${EXECUTABLE_DIR}/config.zsh"
-else
-    print -u 2 -- 'Environment file not found; exiting...'
-    exit 2
-fi
-
+source "${EXECUTABLE_DIR}/tagger-engine"
 local engine_output
-engine_output=$(tagger-engine --verbose --input "${INPUT_DIR}"\
-    --output "${OUTPUT_DIR}" -@ "${ARG_FILES_DIR}/charlesmc.args"\
-    -@ "${ARG_FILES_DIR}/screenshot.args" 2>&1)
+engine_output=$(tagger-engine::main --input "$INPUT_DIR" --output "$OUTPUT_DIR"\
+    -@ "${ARG_FILES_DIR}/charlesmc.args" -@ "${ARG_FILES_DIR}/screenshot.args")
 
 integer -r exit_status=$?
 if (( exit_status == 0 )); then
@@ -71,6 +63,8 @@ else
     subtitle="Failure (Error: $exit_status)"
     sound=Basso
 fi
+
+print -- $engine_output
 
 readonly msg=$(print -- "$engine_output" | tail -n 1)
 osascript <<EOF
